bc85e768b3145877f5bfebc62e7b28130a1acd8a
Cole A Sobotka <cas784@psu.edu>
2018-08-09 12:51:56 -0400

    closes #10

--- pet-clinic-data/src/main/java/com/csobotka/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/csobotka/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package com.csobotka.sfgpetclinic.services;
 
 import com.csobotka.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long id);
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
